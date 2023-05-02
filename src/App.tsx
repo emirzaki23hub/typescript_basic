81e5af6e71b635fe54e8d1d5c0472ad77bcd0ac3
emir <m.emirzaki@iseller.id>
2023-05-02 12:35:04 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import Navbar from "./scenes/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
+import Home from "./scenes/homes";
+import Benefit from "./scenes/benefit";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY == 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefit setSelectedPage={setSelectedPage} />
+    </div>
+  );
+}
+
+export default App;
